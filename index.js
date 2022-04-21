694ce141dcae7257450ac23de96d3ba8ac194616
Wei Luo <wei.luo@snapchat.com>
2022-04-21 14:48:41 -0700

    copy

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PATH_TO_PIXEL_BUNDLE = './static/sc-event.js'
+
+app.get('/scevent.min.js', (req, res) => {
+  res.set('mime-type', 'application/javascript')
+  res.set('Cache-control', 'public, max-age=31536000')
+  res.sendFile(path.join(__dirname, PATH_TO_PIXEL_BUNDLE))
+})
+
+app.get('/p', (req, res) => {
+  console.log('Recvd')
+  console.log(req.body)
+  res.end()
+})
+
+app.listen(8080)
